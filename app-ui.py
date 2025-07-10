92e7e799240fd458bd887817e537a086cac8386f
Tim <153627100+tim-c-1@users.noreply.github.com>
2025-07-10 15:29:00 -0400

    separate tools into tabular views.

--- app-ui.py
+++ app-ui.py
@@ -10,9 +10,23 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("Coordinate Converter")
 
+        tabBar = QTabWidget()
+        tabBar.setTabPosition(QTabWidget.TabPosition.West)
+        tabBar.setMovable(False)
+
+        tabBar.addTab(ddmConvert(), "DDM to DD")
+        tabBar.addTab(transformCoords(), "Transform Coords")
+
+        self.setCentralWidget(tabBar)
+        
+
+class ddmConvert(QWidget):
+    def __init__(self):
+        super().__init__()
+
         #init coord vars
-        self.ddNorthing = ""
-        self.ddEasting = ""
+        ddmConvert.ddNorthing = ""
+        ddmConvert.ddEasting = ""
         self.ddmEasting = ""
         self.ddmNorthing = "" 
 
@@ -29,13 +43,41 @@ class MainWindow(QMainWindow):
         self.ddmOutputBox.setTextInteractionFlags(Qt.TextInteractionFlag.TextSelectableByMouse | Qt.TextInteractionFlag.TextSelectableByKeyboard) #flag for access by mouse and keyboard
         ddmTitle = QLabel("Converter- DDM to DD")
         
+        font = ddmTitle.font()
+        font.setPointSize(22)
+        font.setFamily("Calibri")
+        ddmTitle.setFont(font)
+        ddmTitle.setMaximumHeight(30)
+                
         ddmLayout = QGridLayout()
-        ddmLayout.addWidget(ddmTitle, 0, 0)
+        ddmLayout.addWidget(ddmTitle, 0, 0, 1, 2)
         ddmLayout.addWidget(self.ddmEasting, 1, 0)
         ddmLayout.addWidget(self.ddmNorthing, 1, 1)
         ddmLayout.addWidget(self.ddmBtn, 2, 0)
         ddmLayout.addWidget(self.ddmOutputBox, 2, 1)
+
+        ddmLayout.setContentsMargins(0,0,0,0)
+
+        self.setLayout(ddmLayout)
+
+    def ddm_button_pressed(self):
+        easting = self.ddmEasting.text()
+        northing = self.ddmNorthing.text()
         
+        ddmConvert.ddEasting = coord_converter.Conversions.ddm_to_dd(easting)
+        ddmConvert.ddNorthing = coord_converter.Conversions.ddm_to_dd(northing)
+
+    
+        self.ddmOutputBox.setText(str(ddmConvert.ddEasting) + ' ' + str(ddmConvert.ddNorthing))
+        print(ddmConvert.ddEasting, ddmConvert.ddNorthing)
+
+class transformCoords(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        #init coord vars
+        self.ddLat = ""
+        self.ddLon = ""
 
         #widgets for coord transformation
         self.epsgSource = QComboBox()
@@ -66,10 +108,14 @@ class MainWindow(QMainWindow):
         self.transformOutputBox.setTextInteractionFlags(Qt.TextInteractionFlag.TextSelectableByMouse | Qt.TextInteractionFlag.TextSelectableByKeyboard)
 
         transformTitle = QLabel("Coordinate Transformation")
-
+        font = transformTitle.font()
+        font.setPointSize(22)
+        font.setFamily("Calibri")
+        transformTitle.setFont(font)
+        transformTitle.setMaximumHeight(30)
 
         transformLayout = QGridLayout()
-        transformLayout.addWidget(transformTitle, 0, 0)
+        transformLayout.addWidget(transformTitle, 0, 0, 1, 2)
         transformLayout.addWidget(self.useDDMOut, 1, 0)
         transformLayout.addWidget(self.ddLat, 2, 0)
         transformLayout.addWidget(self.ddLon, 2, 1)
@@ -78,32 +124,15 @@ class MainWindow(QMainWindow):
         transformLayout.addWidget(self.transformBtn, 4, 0)
         transformLayout.addWidget(self.transformOutputBox, 4, 1)
 
-        layout = QVBoxLayout()
-        layout.addLayout(ddmLayout)
-        layout.addLayout(transformLayout)
-
-        widget = QWidget()
-        widget.setLayout(layout)
-        self.setCentralWidget(widget)
-        
-
-    def ddm_button_pressed(self):
-        easting = self.ddmEasting.text()
-        northing = self.ddmNorthing.text()
-        
-        self.ddEasting = coord_converter.Conversions.ddm_to_dd(easting)
-        self.ddNorthing = coord_converter.Conversions.ddm_to_dd(northing)
+        self.setLayout(transformLayout)
 
     
-        self.ddmOutputBox.setText(str(self.ddEasting) + ' ' + str(self.ddNorthing))
-        print(self.ddEasting, self.ddNorthing)
-
     def use_ddm_out_checked(self, s):
         print(s == Qt.CheckState.Checked.value)
         if s == Qt.CheckState.Checked.value:
-            self.ddLat.setText(str(self.ddNorthing))
-            self.ddLon.setText(str(self.ddEasting))
-            print(self.ddNorthing)
+            self.ddLat.setText(str(ddmConvert.ddNorthing))
+            self.ddLon.setText(str(ddmConvert.ddEasting))
+            print(ddmConvert.ddNorthing)
         else:
             self.ddLat.clear()
             self.ddLon.clear()
